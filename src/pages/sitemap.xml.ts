c1d32f79cf8032f64c5e539b290946d5d07315d7
Xavier Geerinck <xavier.geerinck@gmail.com>
2026-01-23 12:54:21 +0100

    fix sitemap
    Signed-off-by: Xavier Geerinck <xavier.geerinck@gmail.com>

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,5 +1,22 @@
 import type { APIRoute } from "astro";
+import fs from "node:fs";
+import path from "node:path";
 
-export const GET: APIRoute = async ({ redirect }) => {
-	return redirect("/sitemap-index.xml", 301);
+export const GET: APIRoute = async () => {
+	// Read the auto-generated sitemap-index.xml from dist
+	const sitemapIndexPath = path.join(process.cwd(), "dist", "sitemap-index.xml");
+	
+	let sitemap: string;
+	try {
+		sitemap = fs.readFileSync(sitemapIndexPath, "utf-8");
+	} catch {
+		// Fallback if file doesn't exist yet
+		sitemap = `<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><sitemap><loc>https://paperlens.io/sitemap-0.xml</loc></sitemap></sitemapindex>`;
+	}
+
+	return new Response(sitemap, {
+		headers: {
+			"Content-Type": "application/xml; charset=utf-8",
+		},
+	});
 };
